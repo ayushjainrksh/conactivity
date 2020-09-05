455620cdbb09920cfb5056d51ff5d89b8147e1c8
Ayush Jain <ayushjainrksh@gmail.com>
2020-09-06 01:41:16 +0530

    Return activity and store it as object

--- scrape.js
+++ scrape.js
@@ -20,8 +20,7 @@ const scrapeLinkedIn = async (data) => {
 
         await page.goto(`https://www.linkedin.com/company/${data.company}`);
         await page.waitFor(3000);
-        await page.click('.mt2');
-
+        await page.click('a.ember-view.link-without-visited-state.inline-block');
         await page.waitFor(3000);
         const result = await page.evaluate(() => {
             console.log('Printing...');
@@ -41,16 +40,37 @@ const scrapeLinkedIn = async (data) => {
         });
         console.log(result);
 
-        result.forEach(async(element) => {
-            await page.goto(element+'detail/recent-activity', {
-                waitUntil: 'networkidle0'
-            })
+        let activity = [];
+        for(let i = 0; i<result.length; i++){
+            let element = result[i];
+            await page.goto(element+'detail/recent-activity');
+            const individualActivity = await page.evaluate(() => {
+                console.log('Found...');
+                let eachActivity = [];
+                if(document.querySelectorAll('div.feed-shared-actor__meta.relative > span.feed-shared-actor__sub-description.t-12.t-black--light.t-normal > div > span.visually-hidden')) {
+                    document.querySelectorAll('div.feed-shared-actor__meta.relative > span.feed-shared-actor__sub-description.t-12.t-black--light.t-normal > div > span.visually-hidden').forEach(item=>{
+                        if(item.innerHTML) {
+                            console.log(item.innerHTML);
+                            if(item.innerHTML.match(/[0-9] (minutes?|hours?|days?|week) ago/))
+                                eachActivity.push(item.innerHTML);
+                        }
+                    });
+                }
+                return eachActivity;
+            });
+            await activity.push({[element]: individualActivity});
+        }
+        let links = [];
+        activity.filter(item=>{
+            if(Object.value(item).length) {
+                links.push(Object.keys(item));
+            }
         });
-
+        console.log(activity);
     }
     catch(err) {
         console.log(err);
     }
   }
 
-  scrapeLinkedIn({username:process.env.EMAIL, password:process.env.PASSWORD, company: "google"});
+  scrapeLinkedIn({username:process.env.EMAIL, password:process.env.PASSWORD, company: "facebook"});
