3529c30c0e37192457edcec2cc5b79ab16451486
Aman Desai <amandesai01@gmail.com>
2020-10-02 17:58:28 +0530

    [FIX] Iterating over Profile list selector

--- scrape.js
+++ scrape.js
@@ -107,11 +107,14 @@ const scrapeLinkedIn = async (data) => {
           ".search-result__info .search-result__result-link",
           ".reusable-search__entity-results-list .entity-result__title-text a",
         ];
-        const profileListNodes =
-          (document.querySelectorAll(profileListSelectors[0]).length &&
-            document.querySelectorAll(profileListSelectors[0])) ||
-          (document.querySelectorAll(profileListSelectors[1]).length &&
-            document.querySelectorAll(profileListSelectors[1]));
+        const profileListNodes = undefined
+        for(let profileListSelectorIndex = 0; profileListSelectorIndex < profileListSelectors; profileListSelectorIndex++){
+          // Breaking Loop when Profile Selector is Found to have data.
+          if(document.querySelectorAll(profileListSelectors[profileListSelectorIndex]).length > 0){
+            profileListNodes = profileListSelectors[profileListSelectorIndex]
+            break;
+          }
+        }
         if (profileListNodes) {
           //Store and return profile links
           let profiles = [];
